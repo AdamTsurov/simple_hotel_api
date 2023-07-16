ff78a75dde17efc39c8f145531d3c6592a83dd72
Adam Tsurov <adamtsurov123@gmail.com>
2023-07-16 12:17:28 +0300

    Update index.js for cors()

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const morgan = require('morgan');
 const cors = require('cors')
 require('dotenv').config();
 
+const corsOptions = {
+  origin: '*', // Разрешить доступ со всех источников
+};
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(require('./routes'))
 
 const { MONGOOSE_URL, PORT } = process.env;
